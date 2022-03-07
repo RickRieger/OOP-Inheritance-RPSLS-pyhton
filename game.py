e9a28043e227650cdf5857f6ed7b50c44d97679d
Rick Rieger <rickrieger36251@gmail.com>
2022-03-07 16:30:54 -0500

    Added is_single_player_true logic

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ class Game:
 
     def run_game(self):
         self.show_greeting()
+        single_player = self.is_single_player_true()
+        print('=====',single_player)
 
     def show_greeting(self):
         print('Welcome to rock, paper, scissors with a twist!\n')
@@ -12,8 +14,16 @@ class Game:
         print("Scissors cuts Paper\nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock\n(and as it always has) Rock crushes Scissors")
         
 
-    def select_single_or_multi(self):
-        pass
+    def is_single_player_true(self):
+        player_choice = ''
+        while player_choice != '1' and player_choice != '2':
+            player_choice = input('Please enter "1" for a single player game, or "2" for a multi player game:')
+            if player_choice != '1' or player_choice != '2':
+                print('Please enter correct value')
+        if player_choice == '1':
+              return True
+        else: return False            
+
 
     def compare_gestures(self):
         pass
