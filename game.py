552aae7370a0e41d52c9b8131e7f9514c5f4cdea
Rick Rieger <rickrieger36251@gmail.com>
2022-03-07 16:10:58 -0500

    Added show greeting func to print greeting and rules to console.

--- game.py
+++ game.py
@@ -3,4 +3,24 @@ class Game:
         pass
 
     def run_game(self):
+        self.show_greeting()
+
+    def show_greeting(self):
+        print('Welcome to rock, paper, scissors with a twist!\n')
+        print('We added two more choices:\nLizard and Spock\n\n')
+        print('Here are the rules:\n')
+        print("Scissors cuts Paper\nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock\n(and as it always has) Rock crushes Scissors")
+        
+
+    def select_single_or_multi(self):
         pass
+
+    def compare_gestures(self):
+        pass
+
+    def check_scores_and_determine_winner(self):
+        pass
+
+    def display_winner(self):
+        pass
+   
