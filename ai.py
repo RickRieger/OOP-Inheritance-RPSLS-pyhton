902d16decc936080e836f7aeb7ab1625b9ed7bf7
tjdanielson <tessajdanielson@gmail.com>
2022-03-07 16:15:27 -0600

    Built out Ai choose_gesture method (will have computer player randomly select a gesture).

--- ai.py
+++ ai.py
@@ -1,5 +1,12 @@
 from player import Player
+import random
 
 class Ai(Player):
     def __init__(self):
         super().__init__()
+
+    def choose_gesture(self):
+        random_number = random.randint(0, 4)
+        computer_choice = self.gestures[random_number]
+        return computer_choice
+
